a05e057945efe59509a0f97e092c7e0b1c65f7cf
Konan N'dassenan Loïc Harold Mehdy <133727447+11Mehdy225@users.noreply.github.com>
2024-12-17 16:38:54 +0000

    Update index.js

--- src/JS/store/index.js
+++ src/JS/store/index.js
@@ -3,3 +3,17 @@ import rootReducer from '../Reducers/rootReducer'
 
 const store = createStore(rootReducer)
 export default store;
+
+// pov please gomycode can you update your courses ?
+
+import { configureStore } from '@reduxjs/toolkit';
+
+import rootReducer from './Reducers/rootReducers'
+
+
+
+const store = configureStore({
+    reducer: rootReducer
+  });
+
+export default store;
